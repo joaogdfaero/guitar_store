3d0d9ce992572967204d8a8781834f45275f290c
João Gabriel Dal Forno <joao.dal@acad.ufsm.br>
2023-09-27 19:21:03 -0300

    feat: chart and line items model

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,12 +1,12 @@
 class Instrument < ApplicationRecord
     belongs_to :user, optional: true
-
+    has_many :line_items
     mount_uploader :image, ImageUploader
 
     validates :title, :brand, :price, :model, presence: true
     validates :description, length: {maximum: 1000, too_long: "%{count} characters is the maximum aloud."}
     validates :title, length: {maximum: 100, too_long: "%{count} characters is the maximum aloud."}
-    validates :price, numericality: {only_integer: true}, length: {maximum: 7}
+    validates :price, length: {maximum: 7}
 
     BRAND = %w{ Fender Gibson Epiphone ESP Martin Dean Taylor Jackson PRS  Ibanez Charvel Washburn }
     FINISH = %w{ Black White Navy Blue Red Clear Satin Yellow Seafoam }
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_23_171134) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_27_221451) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "instruments", force: :cascade do |t|
     t.string "brand"
@@ -28,6 +33,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_23_171134) do
     t.integer "user_id"
   end
 
+  create_table "line_items", force: :cascade do |t|
+    t.bigint "instrument_id", null: false
+    t.bigint "cart_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_line_items_on_cart_id"
+    t.index ["instrument_id"], name: "index_line_items_on_instrument_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -41,4 +55,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_23_171134) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "line_items", "carts"
+  add_foreign_key "line_items", "instruments"
 end
