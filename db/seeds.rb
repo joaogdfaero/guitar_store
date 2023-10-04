372da647721a17d821d12989332bd355508a7a69
João Gabriel Dal Forno <joao.dal@acad.ufsm.br>
2023-10-04 19:42:58 -0300

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,23 +9,23 @@ user = User.new(
   
   Instrument.create!([
     {
-      title: "Paul Reed Smith Paul's Guitar 2013",
+      title: "Guitarra Paul Reed Smith Paul's 2013",
       brand: "PRS",
-      model: "Paul Reed Smith Paul's Guitar",
+      model: "Guitarra Paul Reed Smith Paul's",
       description: "Donec sed odio dui. Maecenas sed diam eget risus varius blandit sit amet non magna.",
-      condition: "Excellent",
-      finish: "Red",
+      condition: "Excelente",
+      finish: "Vermelho",
       price: 2999,
       image: Rails.root.join("app/assets/images/instrument1.jpg").open,
       user_id: user.id
     },
     {
-      title: "2017 Gibson Les Paul Standard Bourbon Burst 100% Mint/Unplayed Condition!",
+      title: "Gibson Les Paul Standard Bourbon Burst 100% Mint/Unplayed Condition!",
       brand: "Gibson",
       model: "Les Paul Standard",
-      description: "Hello and thank you for looking at my Item. We are proud to present this stunning 2018 Gibson Les Paul Standard in Bourbon Burst in 100% Mint/Unplayed condition! The 2017 LP Standards are extremely nice, with super comfortable necks, and awesome bold finishes. This Bourbon Burst finish is absolutely stunning! The tone that comes out of this monster is everything you would expect from a Les Paul Standard!! The guitar sounds awesome and has that famous Les Paul Tone. Very easy to play with a nice comfortable standard neck featuring a compound radius fret board which means shredding speed is easier on a Gibson! This is one great guitar for the money! Will ship via Fed Ex Ground or Home Delivery in Brand New Gibson case with pictured case candy insured for full purchase price!",
-      condition: "Used",
-      finish: "Red",
+      description: "Olá e obrigado por verificar o meu item. Temos o prazer de apresentar esta impressionante Gibson Les Paul Standard 2018 em Bourbon Burst em condições 100% Mint/Unplayed! As Les Paul Standards de 2017 são extremamente agradáveis, com pescoços super confortáveis e acabamentos incríveis e ousados. Este acabamento Bourbon Burst é absolutamente deslumbrante! O tom que sai deste monstro é tudo o que você esperaria de uma Les Paul Standard!! A guitarra soa incrível e tem aquele famoso tom Les Paul. Muito fácil de tocar, com um pescoço padrão confortável com uma escala de raio composto, o que significa que a velocidade de shredding é mais fácil em uma Gibson! Esta é uma ótima guitarra pelo dinheiro! Será enviado via Fed Ex Ground ou Home Delivery em estojo Gibson novo com a imagem de doces do estojo segurado pelo preço total da compra!",
+      condition: "Usado",
+      finish: "Vermelho",
       price: 2595,
       image: Rails.root.join("app/assets/images/instrument2.jpg").open,
       user_id: user.id
@@ -34,9 +34,9 @@ user = User.new(
       title: "Suhr Classic Antique Pro SSS Limited - Surf Green Over 3 Tone Sunburst",
       brand: "Suhr",
       model: "Antique Pro SSS Limited",
-      description: "Our customers have asked for an instrument that has a vintage look and feel, without sacrificing playability and tone. Enter the Classic Antique™. We designed this guitar to ensure it preserves the spirit of a vintage instrument while performing like a Suhr. Impeccable craftsmanship and attention to detail ensure that every neck pocket is tight, every fret is perfectly dressed, and that every instrument is ready for peak performance before leaving our facility. The nitro-cellulose lacquer finish and our proprietary antiquing process make each Classic Antique™ feel like it has been loved for years. The Classic Antique now includes: our innovative SSCII (Silent Single-Coil) hum cancelling system, a vintage tinted nitrocellulose neck with stainless steel frets, and a Maple fingerboard option.",
-      condition: "New",
-      finish: "Seafoam",
+      description: "Nossos clientes pediram por um instrumento que tenha uma aparência e sensação vintage, sem sacrificar a jogabilidade e o tom. Eis o Classic Antique™. Projetamos esta guitarra para garantir que ela preserve o espírito de um instrumento vintage, mantendo o desempenho de uma Suhr. A artesanato impecável e a atenção aos detalhes garantem que cada encaixe de braço seja firme, cada traste esteja perfeitamente alinhado e que cada instrumento esteja pronto para o desempenho máximo antes de sair de nossa instalação. O acabamento de laca de nitrocelulose e nosso processo de envelhecimento proprietário fazem com que cada Classic Antique™ pareça que foi amada por anos. O Classic Antique agora inclui: nosso inovador sistema SSCII (Silent Single-Coil) para cancelamento de ruído, um braço de nitrocelulose tingido vintage com trastes de aço inoxidável e uma opção de escala de bordo.",
+      condition: "Novo",
+      finish: "Verde Mar",
       price: 2845,
       image: Rails.root.join("app/assets/images/instrument3.jpg").open,
       user_id: user.id
@@ -45,9 +45,9 @@ user = User.new(
       title: "Fender American Professional Series Telecaster",
       brand: "Fender",
       model: "Telecaster",
-      description: "The Fender American Professional Series Telecaster brings the company's original electric guitar rocketing into the 21st century with a full complement of upgraded electronics and appointments. Sporting a pair of V-Mod Single-Coil pickups designed by guitar sensei Tim Shaw himself, each pup is custom designed for its placement to get the most out of your neck and bridge. For those who want that trebled Tele scream in lower volume settings, have no fear: the new treble bleed circuit standard on the American Pro Series ensures you get that same presence regardless of where you have the volume knob.",
-      condition: "New",
-      finish: "Clear",
+      description: "A série Fender American Professional Telecaster leva a guitarra elétrica original da empresa para o século 21 com uma gama completa de eletrônicos e acessórios aprimorados. Com um par de captadores V-Mod Single-Coil projetados pelo mestre da guitarra Tim Shaw, cada captador é projetado sob medida para sua posição para obter o máximo proveito do seu braço e ponte. Para aqueles que desejam o grito estridente da Tele em configurações de volume mais baixas, não se preocupe: o novo circuito de redução de agudos padrão na série American Pro garante que você obtenha a mesma presença, independentemente de onde o botão de volume esteja.",
+      condition: "Novo",
+      finish: "Transparente",
       price: 960,
       image: Rails.root.join("app/assets/images/instrument4.jpg").open,
       user_id: user.id
@@ -56,23 +56,104 @@ user = User.new(
       title: "Gibson SG Special",
       brand: "Gibson",
       model: "SG",
-      description: "The Gibson SG Special Faded was born from a variety of small changes made to Gibson instruments over the course of 50-plus years. The SG Special evolved from the Les Paul Special in the early '60s, and was meant to be a less expensive, stripped-down version of the SG Standard for no-frills players. The SG Special Faded, released in 2002 and still in production today, implemented another cost-reducing measure: the use of a light satin finish. Those digging the SG body shape but who don't need unnecessary bells and whistles may want to take a look at the Gibson SG Special Faded.",
-      condition: "Used",
-      finish: "Red",
+      description: "A Gibson SG Special Faded nasceu de uma variedade de pequenas mudanças feitas nos instrumentos da Gibson ao longo de mais de 50 anos. A SG Special evoluiu da Les Paul Special no início dos anos 60 e foi projetada para ser uma versão mais barata e simplificada da SG Standard para músicos que não precisam de recursos extras. A SG Special Faded, lançada em 2002 e ainda em produção hoje, implementou outra medida de redução de custos: o uso de um acabamento acetinado leve. Aqueles que gostam da forma do corpo SG, mas não precisam de sinos e apitos desnecessários, podem querer dar uma olhada na Gibson SG Special Faded.",
+      condition: "Usado",
+      finish: "Vermelho",
       price: 599,
       image: Rails.root.join("app/assets/images/instrument5.jpg").open,
       user_id: user.id
     },
     {
-      title: "Ibanez PM20 Pat Metheny Signature + Hard Shell case",
+      title: "Ibanez PM20 Pat Metheny Signature + Estojo Rígido",
       brand: "Ibanez",
       model: "SG",
-      description: "Path Metheny Signature model guitar in excellent condition. Near zero fret wear and electronics work as expected. Gorgeous natural finish and high quality materials. Back looks amazing as well. No buzz nor issues, low action and very nice tone.",
-      condition: "Used",
-      finish: "Yellow",
+      description: "Modelo de guitarra Pat Metheny Signature em excelente estado. Quase nenhum desgaste nos trastes e eletrônicos funcionando como esperado. Acabamento natural deslumbrante e materiais de alta qualidade. O verso também parece incrível. Sem zumbidos nem problemas, ação baixa e timbre muito agradável.",
+      condition: "Usado",
+      finish: "Amarelo",
       price: 799,
       image: Rails.root.join("app/assets/images/instrument6.jpg").open,
       user_id: user.id
     }
   ])
+
+  
+  user = User.new(
+    id: 3,
+    name: "Fernando",
+    email: "fernando123@example.com",
+    password: "123456",
+    password_confirmation: "123456"
+  )
+  user.save!
+  
+  Instrument.create!([
+    {
+      title: "Guitarra Paul Reed Smith Paul's 2013",
+      brand: "PRS",
+      model: "Guitarra Paul Reed Smith Paul's",
+      description: "Donec sed odio dui. Maecenas sed diam eget risus varius blandit sit amet non magna.",
+      condition: "Excelente",
+      finish: "Vermelho",
+      price: 2999,
+      image: Rails.root.join("app/assets/images/instrument1.jpg").open,
+      user_id: user.id
+    },
+    {
+      title: "Gibson Les Paul Standard Bourbon Burst 100% Mint/Unplayed Condition!",
+      brand: "Gibson",
+      model: "Les Paul Standard",
+      description: "Olá e obrigado por verificar o meu item. Temos o prazer de apresentar esta impressionante Gibson Les Paul Standard 2018 em Bourbon Burst em condições 100% Mint/Unplayed! As Les Paul Standards de 2017 são extremamente agradáveis, com pescoços super confortáveis e acabamentos incríveis e ousados. Este acabamento Bourbon Burst é absolutamente deslumbrante! O tom que sai deste monstro é tudo o que você esperaria de uma Les Paul Standard!! A guitarra soa incrível e tem aquele famoso tom Les Paul. Muito fácil de tocar, com um pescoço padrão confortável com uma escala de raio composto, o que significa que a velocidade de shredding é mais fácil em uma Gibson! Esta é uma ótima guitarra pelo dinheiro! Será enviado via Fed Ex Ground ou Home Delivery em estojo Gibson novo com a imagem de doces do estojo segurado pelo preço total da compra!",
+      condition: "Usado",
+      finish: "Vermelho",
+      price: 2595,
+      image: Rails.root.join("app/assets/images/instrument2.jpg").open,
+      user_id: user.id
+    },
+    {
+      title: "Suhr Classic Antique Pro SSS Limited - Surf Green Over 3 Tone Sunburst",
+      brand: "Suhr",
+      model: "Antique Pro SSS Limited",
+      description: "Nossos clientes pediram por um instrumento que tenha uma aparência e sensação vintage, sem sacrificar a jogabilidade e o tom. Eis o Classic Antique™. Projetamos esta guitarra para garantir que ela preserve o espírito de um instrumento vintage, mantendo o desempenho de uma Suhr. A artesanato impecável e a atenção aos detalhes garantem que cada encaixe de braço seja firme, cada traste esteja perfeitamente alinhado e que cada instrumento esteja pronto para o desempenho máximo antes de sair de nossa instalação. O acabamento de laca de nitrocelulose e nosso processo de envelhecimento proprietário fazem com que cada Classic Antique™ pareça que foi amada por anos. O Classic Antique agora inclui: nosso inovador sistema SSCII (Silent Single-Coil) para cancelamento de ruído, um braço de nitrocelulose tingido vintage com trastes de aço inoxidável e uma opção de escala de bordo.",
+      condition: "Novo",
+      finish: "Verde Mar",
+      price: 2845,
+      image: Rails.root.join("app/assets/images/instrument3.jpg").open,
+      user_id: user.id
+    },
+    {
+      title: "Fender American Professional Series Telecaster",
+      brand: "Fender",
+      model: "Telecaster",
+      description: "A série Fender American Professional Telecaster leva a guitarra elétrica original da empresa para o século 21 com uma gama completa de eletrônicos e acessórios aprimorados. Com um par de captadores V-Mod Single-Coil projetados pelo mestre da guitarra Tim Shaw, cada captador é projetado sob medida para sua posição para obter o máximo proveito do seu braço e ponte. Para aqueles que desejam o grito estridente da Tele em configurações de volume mais baixas, não se preocupe: o novo circuito de redução de agudos padrão na série American Pro garante que você obtenha a mesma presença, independentemente de onde o botão de volume esteja.",
+      condition: "Novo",
+      finish: "Transparente",
+      price: 960,
+      image: Rails.root.join("app/assets/images/instrument4.jpg").open,
+      user_id: user.id
+    },
+    {
+      title: "Gibson SG Special",
+      brand: "Gibson",
+      model: "SG",
+      description: "A Gibson SG Special Faded nasceu de uma variedade de pequenas mudanças feitas nos instrumentos da Gibson ao longo de mais de 50 anos. A SG Special evoluiu da Les Paul Special no início dos anos 60 e foi projetada para ser uma versão mais barata e simplificada da SG Standard para músicos que não precisam de recursos extras. A SG Special Faded, lançada em 2002 e ainda em produção hoje, implementou outra medida de redução de custos: o uso de um acabamento acetinado leve. Aqueles que gostam da forma do corpo SG, mas não precisam de sinos e apitos desnecessários, podem querer dar uma olhada na Gibson SG Special Faded.",
+      condition: "Usado",
+      finish: "Vermelho",
+      price: 599,
+      image: Rails.root.join("app/assets/images/instrument5.jpg").open,
+      user_id: user.id
+    },
+    {
+      title: "Ibanez PM20 Pat Metheny Signature + Estojo Rígido",
+      brand: "Ibanez",
+      model: "SG",
+      description: "Modelo de guitarra Pat Metheny Signature em excelente estado. Quase nenhum desgaste nos trastes e eletrônicos funcionando como esperado. Acabamento natural deslumbrante e materiais de alta qualidade. O verso também parece incrível. Sem zumbidos nem problemas, ação baixa e timbre muito agradável.",
+      condition: "Usado",
+      finish: "Amarelo",
+      price: 799,
+      image: Rails.root.join("app/assets/images/instrument6.jpg").open,
+      user_id: user.id
+    }
+  ])
+
+  
   
